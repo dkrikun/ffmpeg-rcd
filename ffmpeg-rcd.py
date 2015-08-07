64c90954147391be99e3bc649e37f586bb288d09
Daniel Krikun <krikun.daniel@gmail.com>
2015-08-07 18:02:53 +0300

    Add some logging

--- ffmpeg-rcd.py
+++ ffmpeg-rcd.py
@@ -1,3 +1,11 @@
+# coding: utf-8
+
+__version__ = '0.1.0'
+__author__ = 'Daniel Krikun'
+__license__ = 'MIT'
+
+import sys
+import logging
 import psutil
 from subprocess import PIPE
 
@@ -9,6 +17,8 @@ class FfmpegProcess:
 
     def run(self):
         if not self.running:
+            logging.debug('starting ffmpeg with command-line:\n{0}'.format(self._cmdline))
+
             # stdin pipe is required to shutdown ffmpeg gracefully (see below)
             self._process = psutil.Popen(self._cmdline, stdin=PIPE)
 
@@ -17,6 +27,8 @@ class FfmpegProcess:
             self.unpause()
 
         if self.running:
+            logging.debug('stopping ffmpeg process')
+
             # emulate 'q' keyboard press
             self._process.communicate(input='q')
             self._process.wait()
@@ -28,12 +40,16 @@ class FfmpegProcess:
     def pause(self):
         if self.running:
             if not self._paused:
+                logging.debug('suspending ffmpeg process')
+
                 self._process.suspend()
                 self._paused = True
 
     def unpause(self):
         if self.running:
             if self._paused:
+                logging.debug('resuming ffmpeg process')
+
                 self._process.resume()
                 self._paused = False
 
@@ -41,38 +57,50 @@ class FfmpegProcess:
     def paused(self):
         return self._paused
 
+def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG,
+            format='%(message)s')
 
-import msvcrt
-import time
+    import msvcrt
+    import time
 
-audio_device = 'Microphone (High Definition Aud'
-video_device = 'screen-capture-recorder'
-output = 'output.mp4'
+    audio_device = 'Microphone (High Definition Aud'
+    video_device = 'screen-capture-recorder'
+    output = 'output.mp4'
 
-cmdline_template = 'ffmpeg \
--loglevel fatal \
--f dshow -i audio="{0}":video="{1}" \
--vcodec libx264 -pix_fmt yuv420p -preset ultrafast -acodec libmp3lame \
--y {2}'
+    cmdline_template = 'ffmpeg \
+    -loglevel fatal \
+    -f dshow -i audio="{0}":video="{1}" \
+    -vcodec libx264 -pix_fmt yuv420p -preset ultrafast -acodec libmp3lame \
+    -y {2}'
 
-cmdline = cmdline_template.format(audio_device, video_device, output)
-xx = FfmpegProcess(cmdline)
+    cmdline = cmdline_template.format(audio_device, video_device, output)
+    xx = FfmpegProcess(cmdline)
 
 
-cmd = 0
-while True:
-    time.sleep(33./1000.)
-    if not msvcrt.kbhit():
-        continue
+    cmd = 0
+    while True:
+        time.sleep(33./1000.)
+        if not msvcrt.kbhit():
+            continue
 
-    cmd = msvcrt.getch()
-    if cmd == 'r':
-        xx.run()
+        cmd = msvcrt.getch()
+        if cmd == 'r':
+            xx.run()
 
-    elif cmd == 's':
-        xx.stop()
+        elif cmd == 's':
+            xx.stop()
 
-    elif cmd == 'p':
-        xx.pause()
+        elif cmd == 'p':
+            xx.pause()
 
+        elif cmd == '1':
+            xx.print_stats()
+
+        elif cmd == 'Q':
+            xx.stop()
+            break
+
+if __name__ == "__main__":
+    sys.exit(main())
 
